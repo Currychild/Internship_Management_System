eb45c83ce8624fef2467a20a497ce2bd1f272351
Gong Ziyue <gongziyue27@outlook.com>
2024-12-11 22:27:01 +0800

    1.补全外键 2.用户与其他三个角色关系修改完1对1

--- backend/InternshipManagement_sys/my_app/models.py
+++ backend/InternshipManagement_sys/my_app/models.py
@@ -4,14 +4,14 @@ from django.db import models
 # Academy Model (for the 'academy' table)
 
 class Academy(models.Model):
-    a_id = models.IntegerField(primary_key=True, verbose_name='院系Id')
+    a_id = models.IntegerField(primary_key=True, verbose_name='院系ID')
     a_name = models.CharField(max_length=20, verbose_name='院系名称')
     a_profile = models.CharField(max_length=255, verbose_name='院系简介')
     a_dean = models.CharField(max_length=20, verbose_name='院长')
     a_establishdate = models.DateField(verbose_name='成立日期')
     a_conceldate = models.DateField(null=True, blank=True, verbose_name='撤销日期')
     a_lastmodifytime = models.DateTimeField(verbose_name='最后修改时间')
-    a_modifier_id = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='academy_modifier')
+    a_modifier_id = models. ForeignKey('User', on_delete=models.CASCADE, verbose_name='academy_modifier',null=True, blank=True)
 
     class Meta:
         db_table = 'academy'
@@ -21,23 +21,42 @@ class Academy(models.Model):
 
 class Major(models.Model):
     m_id = models.IntegerField(primary_key=True, verbose_name='专业ID')
-    a_id = models.ForeignKey(Academy, on_delete=models.CASCADE, verbose_name='所属院系id')
+    a_id = models.ForeignKey('Academy', on_delete=models.CASCADE, verbose_name='所属院系ID',null=True, blank=True)
     m_name = models.CharField(max_length=20, verbose_name='专业名称')
     m_profile = models.CharField(max_length=255, verbose_name='专业简介')
     m_trainingplan = models.BinaryField(verbose_name='培养计划pdf')
     m_establishdate = models.DateField(verbose_name='成立日期')
     m_conceldate = models.DateField(null=True, blank=True, verbose_name='撤销日期')
     m_lastmodifytime = models.DateTimeField(verbose_name='最后修改时间')
-    m_modifier_id = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='major_modifier')
+    m_modifier_id = models. ForeignKey('User', on_delete=models.CASCADE, verbose_name='major_modifier',null=True, blank=True)
     class Meta:
         db_table = 'major'
         verbose_name = '专业'
         verbose_name_plural = '专业'
 
 
+class Class(models.Model):
+    c_id = models.IntegerField(primary_key=True, verbose_name='班级ID')
+    m_id = models.ForeignKey('Major', on_delete=models.CASCADE, verbose_name='专业ID',null=True, blank=True)
+    t_id = models.ForeignKey('Teacher', on_delete=models.CASCADE, verbose_name='班主任ID',null=True, blank=True)
+    act_id = models.ForeignKey('IpActivity',on_delete=models.CASCADE, verbose_name='活动ID',null=True, blank=True)
+    c_name = models.CharField(max_length=20, verbose_name='班级名称')
+    c_profile = models.CharField(max_length=255, verbose_name='班级简介')
+    c_enrollmentyear = models.IntegerField(verbose_name='入学年份')
+    c_degree = models.CharField(max_length=20, verbose_name='学历层次')
+    c_studentnumber = models.IntegerField(verbose_name='学生人数')
+    c_lastmodifytime = models.DateTimeField(verbose_name='最后修改时间')
+    c_modifier_id = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='class_modifier',null=True, blank=True)
+
+    class Meta:
+        db_table = 'class'
+        verbose_name = '班级'
+        verbose_name_plural = '班级'
+
+
 class Teacher(models.Model):
-    t_id = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='老师ID',primary_key=True)
-    c_id = models.IntegerField(verbose_name='管理班级ID')
+    t_id = models. OneToOneField('User', on_delete=models.CASCADE, verbose_name='老师ID',primary_key=True)
+    c_id = models.ForeignKey('Class',on_delete=models.CASCADE,verbose_name='管理班级ID',null=True, blank=True)
     t_type = models.IntegerField(verbose_name='是否班主任', choices=[(0, '不是班主任'), (1, '是班主任')])
 
     class Meta:
@@ -46,28 +65,9 @@ class Teacher(models.Model):
         verbose_name_plural = '校内导师'
 
 
-class Class(models.Model):
-    c_id = models.IntegerField(primary_key=True, verbose_name='班级id')
-    m_id = models.ForeignKey(Major, on_delete=models.CASCADE, verbose_name='专业ID')
-    t_id = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name='班主任Id')
-    act_id = models.IntegerField(null=True, blank=True, verbose_name='活动ID')
-    c_name = models.CharField(max_length=20, verbose_name='班级名称')
-    c_profile = models.CharField(max_length=255, verbose_name='班级简介')
-    c_enrollmentyear = models.IntegerField(verbose_name='入学年份')
-    c_degree = models.CharField(max_length=20, verbose_name='学历层次')
-    c_studentnumber = models.IntegerField(verbose_name='学生人数')
-    c_lastmodifytime = models.DateTimeField(verbose_name='最后修改时间')
-    c_modifier_id = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='class_modifier')
-
-    class Meta:
-        db_table = 'class'
-        verbose_name = '班级'
-        verbose_name_plural = '班级'
-
-
 class Student(models.Model):
-    s_id = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='学生ID', primary_key=True)
-    c_id = models.ForeignKey(Class, on_delete=models.CASCADE, verbose_name='班级ID')
+    s_id = models. OneToOneField('User', on_delete=models.CASCADE, verbose_name='学生ID', primary_key=True)
+    c_id = models.ForeignKey('Class', on_delete=models.CASCADE, verbose_name='班级ID',null=True, blank=True)
     s_grade = models.CharField(max_length=20, verbose_name='学生年级')
     s_state = models.BinaryField(verbose_name='实习状态，0为否，1为是')
 
@@ -79,7 +79,7 @@ class Student(models.Model):
 
 class Announcement(models.Model):
     an_id = models.IntegerField(primary_key=True, verbose_name='公告id')
-    an_publisher_id = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='announcement_publisher')
+    an_publisher_id = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='announcement_publisher',null=True, blank=True)
     an_title = models.CharField(max_length=200, verbose_name='公告标题')
     an_content = models.TextField(verbose_name='公告内容')
     an_topddl = models.DateField(verbose_name='置顶截止时间')
@@ -107,9 +107,9 @@ class Company(models.Model):
 
 
 class CompanyTeacher(models.Model):
-    cot_id = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='企业导师ID', primary_key=True)
-    co_id = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='企业ID')
-    act_id = models.IntegerField(null=True, blank=True, verbose_name='管理的实习活动id')
+    cot_id = models. OneToOneField('User', on_delete=models.CASCADE, verbose_name='企业导师ID', primary_key=True)
+    co_id = models.ForeignKey('Company', on_delete=models.CASCADE, verbose_name='企业ID',null=True, blank=True)
+    act_id = models.ForeignKey('IpActivity',on_delete=models.CASCADE,null=True, blank=True, verbose_name='管理的实习活动id')
     cot_title = models.CharField(max_length=45, verbose_name='职称/头衔')
 
     class Meta:
@@ -119,8 +119,8 @@ class CompanyTeacher(models.Model):
 
 
 class IpActivity(models.Model):
-    act_id = models.IntegerField(primary_key=True, verbose_name='实习活动id')
-    co_id = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='企业id')
+    act_id = models.IntegerField(primary_key=True, verbose_name='实习活动ID')
+    co_id = models.ForeignKey('Company', on_delete=models.CASCADE, verbose_name='企业id',null=True, blank=True)
     act_name = models.CharField(max_length=255, verbose_name='实习活动名称')
     act_description = models.CharField(max_length=255, verbose_name='实习活动描述')
     act_begindate = models.DateField(verbose_name='实习开始日期')
@@ -133,9 +133,9 @@ class IpActivity(models.Model):
 
 
 class IpApplication(models.Model):
-    appl_id = models.IntegerField(primary_key=True, verbose_name='实习申请表id')
-    s_id = models.IntegerField(verbose_name='学生id')
-    j_id = models.IntegerField(verbose_name='实习岗位id')
+    appl_id = models.IntegerField(primary_key=True, verbose_name='实习申请表ID')
+    s_id = models.ForeignKey('Student',on_delete=models.CASCADE,verbose_name='学生ID',null=True, blank=True)
+    j_id = models.ForeignKey('IpJob',on_delete=models.CASCADE,verbose_name='实习岗位ID',null=True, blank=True)
     appl_resume = models.BinaryField(verbose_name='简历pdf')
     appl_choice = models.IntegerField(verbose_name='志愿编号')
     appl_status = models.IntegerField(verbose_name='申请表状态')
@@ -148,8 +148,8 @@ class IpApplication(models.Model):
 
 class IpJob(models.Model):
     j_id = models.IntegerField(primary_key=True, verbose_name='岗位ID')
-    act_id = models.ForeignKey(IpActivity, on_delete=models.CASCADE, verbose_name='实习活动ID')
-    co_id = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='公司ID')
+    act_id = models.ForeignKey('IpActivity', on_delete=models.CASCADE, verbose_name='实习活动ID',null=True, blank=True)
+    co_id = models.ForeignKey('Company', on_delete=models.CASCADE, verbose_name='公司ID',null=True, blank=True)
     j_name = models.CharField(max_length=20, verbose_name='岗位名称')
     j_description = models.CharField(max_length=255, verbose_name='岗位描述')
     j_salary = models.FloatField(verbose_name='实习工资')
@@ -165,10 +165,10 @@ class IpJob(models.Model):
 
 
 class IpSummary(models.Model):
-    sm_id = models.IntegerField(primary_key=True, verbose_name='实习总结id')
-    s_id = models.IntegerField(verbose_name='学生ID')
-    t_id = models.IntegerField(verbose_name='校内导师id')
-    cot_id = models.IntegerField(verbose_name='企业导师id')
+    sm_id = models.IntegerField(primary_key=True, verbose_name='实习总结ID')
+    s_id = models.ForeignKey('Student',on_delete=models.CASCADE,verbose_name='学生ID',null=True, blank=True)
+    t_id = models.ForeignKey('Teacher',on_delete=models.CASCADE,verbose_name='校内导师ID',null=True, blank=True)
+    cot_id = models.ForeignKey('CompanyTeacher',on_delete=models.CASCADE,verbose_name='企业导师ID',null=True, blank=True)
     sm_submissiontime = models.DateTimeField(verbose_name='提交时间')
     sm_content = models.BinaryField(verbose_name='实习总结pdf')
     sm_t_score = models.IntegerField(null=True, blank=True, verbose_name='校内导师给分')
@@ -184,10 +184,10 @@ class IpSummary(models.Model):
 
 
 class IpWeekly(models.Model):
-    w_id = models.IntegerField(primary_key=True, verbose_name='实习周报id')
-    s_id = models.IntegerField(verbose_name='学生ID')
-    t_id = models.IntegerField(verbose_name='校内导师id')
-    cot_id = models.IntegerField(verbose_name='企业导师id')
+    w_id = models.IntegerField(primary_key=True, verbose_name='实习周报ID')
+    s_id = models.ForeignKey('Student',on_delete=models.CASCADE,verbose_name='学生ID',null=True, blank=True)
+    t_id = models.ForeignKey('Teacher',on_delete=models.CASCADE,verbose_name='校内导师ID',null=True, blank=True)
+    cot_id = models.ForeignKey('CompanyTeacher',on_delete=models.CASCADE,verbose_name='企业导师ID',null=True, blank=True)
     w_content = models.BinaryField(verbose_name='周报pdf')
     w_time = models.CharField(max_length=45, verbose_name='实习周次')
     w_submissiontime = models.DateTimeField(verbose_name='周报提交时间')
